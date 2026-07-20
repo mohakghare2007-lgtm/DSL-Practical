5c614dfc81ed8e01aa14b02fb0c3aed181983d79
mohakghare2007-lgtm <mohakghare2007@gmail.com>
2026-07-20 22:32:20 +0530

    Add files via upload
    Practical programs of 13-06-2026

---
+++ desktop-tutorial/demo.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Name:-Mohak Mangesh Ghare";
+    cout<<"Computer engineering student";
+    cout<<"Talegaon Dabhade";
+    return 0;
+}
---
+++ desktop-tutorial/sample.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Welcome toWorld of Hacking";
+    return 0;
+}
